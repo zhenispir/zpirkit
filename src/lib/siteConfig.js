e653d5b21b30c13bfd5607a10812bf7c1b266ac8
Zhenis Pir <yenispiro@gmail.com>
2024-08-18 19:27:15 +0500

    Changed siteConfig.js with my own values

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -1,12 +1,12 @@
-export const SITE_URL = 'https://swyxkit.netlify.app';
-export const APPROVED_POSTERS_GH_USERNAME = ['swyxio'];
-export const GH_USER_REPO = 'swyxio/swyxkit'; // used for pulling github issues and offering comments
-export const SITE_TITLE = 'SwyxKit';
-export const SITE_DESCRIPTION = "swyx's default SvelteKit + Tailwind starter";
+export const SITE_URL = 'https://www.zhenispir.com';
+export const APPROVED_POSTERS_GH_USERNAME = ['zhenispir'];
+export const GH_USER_REPO = 'zhenispir/my-swyxkit'; // used for pulling github issues and offering comments
+export const SITE_TITLE = 'ZhenisPir';
+export const SITE_DESCRIPTION = "My personal blog. I'm a teacher, web developer, writer, and youtuber.";
 export const DEFAULT_OG_IMAGE =
-	'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';
-export const MY_TWITTER_HANDLE = 'swyx';
-export const MY_YOUTUBE = 'https://youtube.com/swyxTV';
+	'';
+export const MY_TWITTER_HANDLE = 'ZhenisPir';
+export const MY_YOUTUBE = '';
 export const POST_CATEGORIES = ['Essay', 'Note']; // Other categories you can consider adding: Talks, Tutorials, Snippets, Podcasts, Notes...
 export const GH_PUBLISHED_TAGS = ['Published'];
 
